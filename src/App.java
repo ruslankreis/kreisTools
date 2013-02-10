559e9aeb41244c1c75e169fc75b8c26fc6070e9e
Ruslan K. <forproplay@gmail.com>
2013-02-10 19:31:44 +0400

    Branch

--- src/App.java
+++ src/App.java
@@ -12,20 +12,6 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
-        final List<Integer> ls = new ArrayList<>(100);
-        DSL.init(ls, 0, 100, false);
 
-        List<Tuple2<Integer, Integer>> a = DSL.mapi(ls, new Func2<Integer, Integer>() {
-            public Integer f(Integer src) {
-                return src;
-            }
-        });
-        a = DSL.filter(a, new Predicate<Tuple2<Integer, Integer>>() {
-            public boolean f(Tuple2<Integer, Integer> src) {
-                return src.get_first() % 5 == 0 || src.get_first() % 7 == 0;
-            }
-        });
-
-        System.out.println(a);
     }
 }
